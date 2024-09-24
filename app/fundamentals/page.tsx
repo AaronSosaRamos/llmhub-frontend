4d96610dd9e27bd68f326ddb32b819280b5e8594
WilfredoAaronSosaRamosBoushtech <wilfredo.sosa@boushtech.net>
2024-09-24 08:24:58 -0500

    new feature/fundamentals-main-page

---
+++ app/fundamentals/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import FundamentalsScreen from '@/components/fundamentals/FundamentalsSection';
+import Layout from '@/components/Layout';
+
+export default function Home() {
+  return (
+    <Layout>
+      <FundamentalsScreen />
+    </Layout>
+  );
+}
