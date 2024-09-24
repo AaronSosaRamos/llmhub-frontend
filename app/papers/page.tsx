302b33670461fbb7e2a4a2f56412b4ed90c6f770
WilfredoAaronSosaRamosBoushtech <U20204320@gmail.com>
2024-09-24 09:40:22 -0500

    new papers main section imp.

---
+++ app/papers/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import Layout from '@/components/Layout';
+import PapersScreen from '@/components/papers/PapersSection';
+
+export default function Home() {
+  return (
+    <Layout>
+      <PapersScreen />
+    </Layout>
+  );
+}
