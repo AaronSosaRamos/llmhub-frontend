3cabff675c072f1f51b862ce8d968b1fa322d258
WilfredoAaronSosaRamosBoushtech <U20204320@gmail.com>
2024-09-24 11:14:38 -0500

    feature/inference-optimization

---
+++ app/inference-optimization/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import InferenceOptimizationScreen from '@/components/inference-optimization/InferenceOptimizationSection';
+import Layout from '@/components/Layout';
+
+export default function Home() {
+  return (
+    <Layout>
+      <InferenceOptimizationScreen />
+    </Layout>
+  );
+}
