0130d83d2c8a08f8b88ca0ea748cb7804591cbb5
WilfredoAaronSosaRamosBoushtech <U20204320@gmail.com>
2024-09-24 09:46:42 -0500

    feature/open-source-main-screen

---
+++ app/open-source/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import Layout from '@/components/Layout';
+import OpenSourceScreen from '@/components/open-source/OpenSourceSection';
+
+export default function Home() {
+  return (
+    <Layout>
+      <OpenSourceScreen />
+    </Layout>
+  );
+}
